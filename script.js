93b165ba13d07bdba5d3aa30d81ab85536167b4e
Raj-fullstack <RPURUSH5@GMAIL.COM>
2025-10-08 19:19:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 
-const $ = (selector) => document.querySelector(selector);
-const $$ = (selector) => document.querySelectorAll(selector);
+const qselect = (selector) => document.querySelector(selector);
+const qselectall = (selector) => document.querySelectorAll(selector);
 
 //Popup open
-$(".green_btn").addEventListener("click", () => togglePopup("income"));
-$(".red_btn").addEventListener("click", () => togglePopup("expense"));
+qselect(".add_income").addEventListener("click", () => togglePopup("income"));
+qselect(".add_expense").addEventListener("click", () => togglePopup("expense"));
 //cancel Btn
-$(".cancel_income").addEventListener("click", closePopup);
-$(".cancel_expense").addEventListener("click", closePopup);
+[".cancel_income", ".cancel_expense"].forEach(sel => qselect(sel).addEventListener("click", closePopup));
 // Save btn
-$(".save_btn").addEventListener("click", () => handleTransaction("income"));
-$(".expense_update").addEventListener("click", () => handleTransaction("expense"));
+qselect(".save_btn").addEventListener("click", () => handleTransaction("income"));
+qselect(".expense_update").addEventListener("click", () => handleTransaction("expense"));
 //popup 
 function togglePopup(type) {
-  $(".popup_wrapper").style.display = "flex";
-  $(".income").style.display = type === "income" ? "flex" : "none";
-  $(".expense").style.display = type === "expense" ? "flex" : "none";
+  qselect(".popup_wrapper").style.display = "flex";
+  qselect(".income").style.display = type === "income" ? "flex" : "none";
+  qselect(".expense").style.display = type === "expense" ? "flex" : "none";
 }
 function closePopup() {
-  $(".popup_wrapper").style.display = "none";
+  qselect(".popup_wrapper").style.display = "none";
   clearInputs();
+  qselect(".expense_update").classList.remove("save_expenses");
+  qselect(".save_update").classList.remove("save_income");
 }
 
 // inocome or expense type check
 function handleTransaction(type) {
   let index = type == "income" ? 0 : 1;
  
-  let amountElem = $$(".amount_value")[index];// document.getElementbyclassname("amount")
-  let descElem = $$(".description_value")[index];
+  let amountElem = qselectall(".amount_value")[index];// document.getElementbyclassname("amount")
+  let descElem = qselectall(".description_value")[index];
 
   let net = amountElem.value.trim();
   console.log(net)
@@ -47,7 +48,7 @@ function handleTransaction(type) {
   }
   trans_list(net, description, color, type);
   //update bal
-  const totalElem = $(".total_amount");
+  const totalElem = qselect(".total_amount");
   const total = parseInt(totalElem.innerText) || 0;
   totalElem.innerText = total + sign * parseInt(net);
   closePopup();
@@ -60,11 +61,15 @@ function trans_list(amount, description, color, type) {
   list.className = `list_container ${color}`;
 
   let descDiv = document.createElement("div");
+  descDiv.setAttribute("class","list-desc");
   descDiv.textContent = description;
 
   let amountDiv = document.createElement("div");
+  let amount_val = document.createElement("div");
   amountDiv.className = "amount-trans";
-  amountDiv.innerHTML = amount;
+  amount_val.className = "amount-val";
+  amount_val.innerHTML = amount;
+  amountDiv.append(amount_val);
 
   let iconDiv = document.createElement("div");
   iconDiv.className = "dot";
@@ -85,16 +90,16 @@ function trans_list(amount, description, color, type) {
   amountDiv.append(iconDiv);
 
   list.append(descDiv, amountDiv);
-  $(".trans_data").append(list);
+  qselect(".trans_data").append(list);
    //delete
    deleteItem.addEventListener("click", deleteItems);
  
   function deleteItems(){
-  console.log($(".total_amount").innerText);
-   let getAmount=parseInt( $(".total_amount").innerText);
+  console.log(qselect(".total_amount").innerText);
+   let getAmount=parseInt( qselect(".total_amount").innerText);
    let net = parseInt(amount);
    let subt=net + getAmount;
-   $(".total_amount").innerText = subt;
+   qselect(".total_amount").innerText = subt;
  console.log(subt);
 
     list.remove();
@@ -104,7 +109,8 @@ function trans_list(amount, description, color, type) {
   
 editItem.addEventListener("click", function(){
   var list_type = list.getAttribute("list-type");
-  editList(list_type,description,amount);
+  console.log(this);
+  editList(list_type,description,amount,this);
   list.remove();
 });
 //menu
@@ -137,21 +143,59 @@ function showError(inputElem, message) {
   });
 }
 
-  function editList(type,des,amount){
-  $(".popup_wrapper").style.display = "flex";
-  $(".income").style.display = type === "income" ? "flex" : "none";
-  $(".expense").style.display = type === "expense" ? "flex" : "none";
+  function editList(type,des,amount,ele){
+  qselect(".popup_wrapper").style.display = "flex";
+  qselect(".income").style.display = type === "income" ? "flex" : "none";
+  qselect(".expense").style.display = type === "expense" ? "flex" : "none";
+  qselect(".expense_update").classList.add("save_expenses");
+  qselect(".save_update").classList.add("save_income");
+  var bal = qselect(".total_amount").innerText;
+
+  var edit_selector = ele.parentNode.parentNode.parentNode;
+
+  console.log(edit_selector);
 
   let index = type === "income" ? 0 : 1;
-  let amountElem = $$(".amount_value")[index];
-  let descElem = $$(".description_value")[index];
+  let amountElem1 = qselectall(".amount_value")[index];
+  let descElem1 = qselectall(".description_value")[index];
 
-   amountElem.value = amount;
-   descElem.value = des;
+   amountElem1.value = amount;
+   descElem1.value = des;
+
+   qselect(".save_income").addEventListener("click",function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    window.stop();
+    console.log(e);
+    let color = type === "income" ? "color1" : "color2";
+  let sign = type === "income" ? -1 : 1;
+    //let index = type == "income" ? 0 : 1;
+ 
+  //let amountElem = qselectall(".amount_value")[index];// document.getElementbyclassname("amount")
+  //let descElem = qselectall(".description_value")[index].innerText;
+  console.log(amountElem1);
+
+    //edit_selector.closest(".list-desc").innerText = descElem;
+    console.log(edit_selector.matches(".amount-val"));
+    edit_selector.matches(".amount-val").innerText = amountElem1;
+
+    console.log("bal "+bal);
+    console.log("amount "+amountElem1);
+    console.log("total "+parseInt(bal) -  parseInt(amountElem1))
+
+    bal.innerText = parseInt(bal) -  parseInt(amountElem1);
+
+    //const totalElem = qselect(".total_amount");
+  //const total = parseInt(bal.innerText) || 0;
+  //bal.innerText = total + sign * parseInt(amountElem);
+    
+  });
   }
+
+  
 
 // Reset inputs
 function clearInputs() {
-  $$(".amount_value").forEach((i) => (i.value = ""));
-  $$(".description_value").forEach((i) => (i.value = ""));
+  qselectall(".amount_value").forEach((i) => (i.value = ""));
+  qselectall(".description_value").forEach((i) => (i.value = ""));
 }
